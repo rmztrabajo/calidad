2a7b2146eefa881efa507f5f05b08b089bd02261
rmztrabajo <osvaldormztrabajo@gmail.com>
2025-07-14 19:40:29 -0600

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+
+  if (!token && request.nextUrl.pathname.startsWith("/protected")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/protected/:path*"],
+};
